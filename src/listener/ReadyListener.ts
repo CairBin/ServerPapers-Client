db5b8fd07e4fa7272a6c6096d5682b5dbbcf9967
CairBin <cairbin@CairBindeMacBook-Pro.local>
2024-12-01 14:50:14 +0800

    perf: Adjust code structure

--- src/listener/ReadyListener.ts
+++ src/listener/ReadyListener.ts
@@ -16,12 +16,13 @@ import { inject, injectable } from "inversify";
 import IStateHelper from "../status/IStateHelper";
 import config from 'config';
 import IHash from "../crypto/IHash";
+import { InfoData } from "../status/StateFormat";
 
 // 发送信息的格式
 interface ClientMessage{
     user:string;
     token:string;
-    data:Object;
+    data:InfoData;
 }
 
 @injectable()
@@ -46,14 +47,7 @@ export default class ReadyListener implements ISocketListener{
         // 若定时器不存在则启用一个，每个1000ms返回发送一次信息
         this.interval = setInterval(async ()=>{
             const sendMsg:ClientMessage = {
-                data:{
-                    cpu: await this.stateHelper.getCpuInfo(),
-                    system: await this.stateHelper.getSystemInfo(),
-                    memory: await this.stateHelper.getMemoryInfo(),
-                    network: await this.stateHelper.getNetworkInfo(),
-                    disk: await this.stateHelper.getDiskInfo(),
-                    time: await this.stateHelper.getTimeInfo()
-                },
+                data: await this.stateHelper.getInfoData(),
                 user:config.get<string>('user'),
                 token: this.hash.hash(config.get<string>('pwd')),
             }
