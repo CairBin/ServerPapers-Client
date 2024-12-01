f8da6a7529570952ea6380d89e32f2aa8d21a960
CairBin <cairbin@CairBindeMacBook-Pro.local>
2024-12-01 15:49:42 +0800

    fix: Fixed some bugs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,7 @@ module.exports = {
       name: 'ServerPapersClient',
       script: 'build/app.js', // 编译后的入口文件
       watch: false, // 生产环境通常关闭监听
+      node_args: '--experimental-specifier-resolution=node',
       env: {
         NODE_ENV: 'production',
       },
